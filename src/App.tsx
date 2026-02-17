298536f93b6b68b95c28084233ad4e6301173691
Itsalexfr1 <Itsalexfr1@users.noreply.github.com>
2026-02-17 13:34:20 +0100

    fix: switch to HashRouter to resolve routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
 import { News } from './pages/News';
